ab013abd8075db37309e0601594fa44c33bd48d1
Michael Shields <michael.shields@keysight.com>
2021-10-27 11:17:33 -0600

    remove page not found in start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HtmlTableComponent } from './html-table/html-table.component';
-import { MaterialTableComponent } from './material-table/material-table.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
 
